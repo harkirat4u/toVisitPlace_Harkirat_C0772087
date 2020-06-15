3b60699db7441d91c94dfb7725e1e49e7d46f510
harkirat singh <singhharkirat051@gmail.com>
2020-06-14 23:20:32 -0400

    Initial Commit

---
+++ toVisitPlace_Harkirat_C0772087/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toVisitPlace_Harkirat_C0772087
+//
+//  Created by Harkirat singh on 2020-06-14.
+//  Copyright © 2020 Harkirat singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
